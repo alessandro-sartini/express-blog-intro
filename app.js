9533d65143260274c4a9e7e4f2b9647835f4dae7
alessandro-sartini <ale.sartini.98@gmail.com>
2025-02-10 12:09:39 +0100

    add: moduls

--- app.js
+++ app.js
@@ -5,7 +5,13 @@ const port = 3000;
 
 app.get(`/`, (req, res) => {
     
+res.send('try connection')
 
 
+});
+
+app.listen(port, () => {
     
-}  )
+    console.log(`App in esecuzione alla porta ${port}`)
+
+})
